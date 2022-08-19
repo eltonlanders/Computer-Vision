7a25326bd30f479886d6018e7d2ee3daed444d4e
Elton <57378191+eltonlanders@users.noreply.github.com>
2022-08-19 14:53:11 +0530

    Add files via upload

---
+++ YOLOv5/json_to_text.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 15 09:48:11 2022
+
+@author: Elton Landers
+"""
+import json
+import os
+
+
+
+name2id = {"label_1": 0, "label_2": 1}  # Label dict with names
+
+def convert(img_size, box):
+    dw = 1. / (img_size[0])
+    dh = 1. / (img_size[1])
+    x = (box[0] + box[2]) / 2.0 - 1
+    y = (box[1] + box[3]) / 2.0 - 1
+    w = box[2] - box[0]
+    h = box[3] - box[1]
+    x = x * dw
+    w = w * dw
+    y = y * dh
+    h = h * dh
+    return (x, y, w, h)
+
+def decode_json(json_floder_path, json_name):
+    txt_name = "C:/Users/gjy3r6/Documents/yolo_custom_data/dataset (3)/labels_text/" + json_name[0:-5] + ".txt" # Absolute path of the folder which will contain text files
+    txt_file = open(txt_name, 'w')
+
+    json_path = os.path.join(json_floder_path, json_name)
+    data = json.load(open(json_path, 'r', encoding='gb2312', errors='ignore'))
+
+    img_w = data['imageWidth']
+    img_h = data['imageHeight']
+
+    for i in data['shapes']:
+
+        label_name = i['label']
+        if (i['shape_type'] == 'rectangle'):
+            x1 = int(i['points'][0][0])
+            y1 = int(i['points'][0][1])
+            x2 = int(i['points'][1][0])
+            y2 = int(i['points'][1][1])
+
+            bb = (x1, y1, x2, y2)
+            bbox = convert((img_w, img_h), bb)
+            txt_file.write(str(name2id[label_name]) + " " + " ".join([str(a) for a in bbox]) + '\n')
+
+
+if __name__ == "__main__":
+    json_folder_path = " " # Absolute path of the folder which contains JSONs 
+    # json_names = os.listdir(json_folder_path)
+    json_names = [names for names in os.listdir(json_folder_path) if names.endswith('.json')]
+    for json_name in json_names:
+        decode_json(json_folder_path, json_name)
+        
+        
