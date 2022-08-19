fb15c59f16096e4e5f225061e471fbc8bf5163cb
Elton <57378191+eltonlanders@users.noreply.github.com>
2022-08-19 15:26:09 +0530

    Add files via upload

---
+++ YOLOv5/default_inference.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 19 15:23:32 2022
+
+@author: Elton Landers
+"""
+
+!python detect.py --source path_to.mp4 --weights runs/train/custom_run_08/weights/best.pt
